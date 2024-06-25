542d7c794c3a4fee64c870d71b0cb6538d677e49
kimminam <bluemind8520@gmail.com>
2024-06-25 22:45:16 +0900

    Feat. Title Css 및 다크모드 토글 스위치 - Header 컨테이너 생성 및 컨테이너 CSS 적용 - Title 폰트 굵기 세미볼드 - 다크 모드 토글 스위치 생성

--- koreanpersonalitynotes/components/Header.tsx
+++ koreanpersonalitynotes/components/Header.tsx
@@ -1,22 +1,35 @@
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View, Switch } from 'react-native'
+import { useState } from 'react'
 
 const Header = () => {
+  const [isEnabled, setIsEnabled] = useState(false)
+  const darkModeToggle = () => setIsEnabled(previousState => !previousState)
   return (
-    <Text style={styles.titleBox}>
-      <Text style={styles.title}>Notes</Text>;
-    </Text>
-  );
-};
+    <View style={styles.headerContainer}>
+      <Text style={styles.title}>Notes</Text>
+      <Switch
+        trackColor={{ false: '#767577', true: '#81b0ff' }}
+        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={darkModeToggle}
+        value={isEnabled}
+      />
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
-  titleBox: {
-    flex: 1,
-    justifyContent: 'flex-start',
+  headerContainer: {
+    width: '100%',
+    padding: 10,
+    display: 'flex',
+    flexDirection: 'row',
   },
   title: {
     color: '#fff',
     fontSize: 30,
+    fontWeight: 'semibold',
   },
-});
+})
 
-export default Header;
+export default Header
