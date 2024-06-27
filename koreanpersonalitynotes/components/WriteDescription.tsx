f1738b04e4013a89edbb3762d6eb5a60ab0502c8
kimminam <bluemind8520@gmail.com>
2024-06-27 22:33:09 +0900

    Add. 키워드 내용 작성 컨포넌트 - 키워드에 맞게 작성하는 내용 컨포넌트

---
+++ koreanpersonalitynotes/components/WriteDescription.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet, View, TextInput } from 'react-native'
+
+const WriteDescription = () => {
+  return (
+    <View style={styles.WriteDescriptionContainer}>
+      <TextInput
+        style={styles.descriptionInput}
+        placeholder="키워드에 맞게 자신의 이야기를 적어주세요!"
+        placeholderTextColor={'White'}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  WriteDescriptionContainer: {
+    marginTop: 20,
+    height: 550,
+    borderBottomWidth: 1,
+    borderBlockColor: '#2D2D2D',
+  },
+  descriptionInput: {
+    color: '#fff',
+  },
+})
+
+export default WriteDescription
