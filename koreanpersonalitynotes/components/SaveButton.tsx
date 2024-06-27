b8a54b56f958c3c3af8674be194bd1cd752cece5
kimminam <bluemind8520@gmail.com>
2024-06-27 22:32:33 +0900

    Add. 저장 버튼 컨포넌트 - 임시저장 및 저장하기 버튼 컨포넌트

---
+++ koreanpersonalitynotes/components/SaveButton.tsx
@@ -0,0 +1,47 @@
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native'
+
+const SaveButton = () => {
+  return (
+    <View style={styles.saveContainer}>
+      <TouchableOpacity style={styles.draftsButton}>
+        <Text style={styles.draftsText}>임시저장</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.saveButton}>
+        <Text style={styles.saveText}>저장하기</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  saveContainer: {
+    marginTop: 30,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  draftsButton: {
+    backgroundColor: '#7A7A7A',
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+    borderRadius: 6,
+  },
+  saveButton: {
+    backgroundColor: '#47D644',
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+    borderRadius: 6,
+  },
+  draftsText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  saveText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+})
+
+export default SaveButton
