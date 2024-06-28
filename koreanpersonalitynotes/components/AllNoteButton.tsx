babeb5ca82ced9f1c761afb0a4ecf99b990a801c
kimminam <bluemind8520@gmail.com>
2024-06-28 22:42:39 +0900

    Feat. 버튼 width 지정

--- koreanpersonalitynotes/components/AllNoteButton.tsx
+++ koreanpersonalitynotes/components/AllNoteButton.tsx
@@ -1,16 +1,16 @@
-import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Text } from 'react-native'
 import {
   NavigationProp,
   ParamListBase,
   useNavigation,
-} from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/Ionicons';
+} from '@react-navigation/native'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 const AllNotesButton = () => {
-  const navigation = useNavigation<NavigationProp<ParamListBase>>();
+  const navigation = useNavigation<NavigationProp<ParamListBase>>()
   const handleBack = () => {
-    navigation.navigate('Home');
-  };
+    navigation.navigate('Home')
+  }
 
   return (
     <View style={styles.AllNotesButtonView}>
@@ -24,17 +24,19 @@ const AllNotesButton = () => {
         <Text style={styles.backText}>All Notes</Text>
       </TouchableOpacity>
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   AllNotesButtonView: {
+    width: '100%',
     paddingBottom: 10,
     borderBottomWidth: 1,
     borderBlockColor: '#2D2D2D',
     paddingTop: 10,
   },
   backButton: {
+    width: '32%',
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
@@ -43,6 +45,6 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: '#fff',
   },
-});
+})
 
-export default AllNotesButton;
+export default AllNotesButton
