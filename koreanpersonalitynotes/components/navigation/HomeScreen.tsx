61d18c1d7b09806e43b4706e749c0006ad8df216
kimminam <bluemind8520@gmail.com>
2024-07-02 23:41:39 +0900

    Feat : IOS 최적화 삭제
    SafeAreaView 삭제

--- koreanpersonalitynotes/components/navigation/HomeScreen.tsx
+++ koreanpersonalitynotes/components/navigation/HomeScreen.tsx
@@ -1,78 +1,78 @@
-import { View, StyleSheet, ScrollView, SafeAreaView } from 'react-native';
-import Header from '@/components/Header';
-import SearchBar from '@/components/SearchBar';
-import NotesList from '@/components/NotesList';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useState, useEffect } from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native'
+import Header from '@/components/Header'
+import SearchBar from '@/components/SearchBar'
+import NotesList from '@/components/NotesList'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useState, useEffect } from 'react'
 
 interface Item {
-  personality: string;
-  description: string;
-  createAt: string;
-  saveOrDrafts: string;
+  personality: string
+  description: string
+  createAt: string
+  saveOrDrafts: string
 }
 
 export default function HomeScreen() {
-  const [items, setItems] = useState<Item[]>([]);
+  const [items, setItems] = useState<Item[]>([])
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const keys = await AsyncStorage.getAllKeys();
-        const result = await AsyncStorage.multiGet(keys);
+        const keys = await AsyncStorage.getAllKeys()
+        const result = await AsyncStorage.multiGet(keys)
         const parsedItems = result
           .map(([key, value]) => {
             if (value) {
-              return JSON.parse(value) as Item;
+              return JSON.parse(value) as Item
             }
-            return null;
+            return null
           })
-          .filter(item => item !== null) as Item[];
-        setItems(parsedItems);
+          .filter(item => item !== null) as Item[]
+        setItems(parsedItems)
       } catch (error) {
-        console.error('Error retrieving data from AsyncStorage:', error);
+        console.error('Error retrieving data from AsyncStorage:', error)
       }
-    };
+    }
 
-    fetchData();
-  });
+    fetchData()
+  })
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.containerBox}>
-        <View>
-          <Header />
-          <SearchBar />
-          <ScrollView style={styles.notesContainer}>
-            {items.map((item, index) => (
-              <NotesList
-                key={index}
-                personality={item.personality}
-                create_at={item.createAt}
-                notesStatus={item.saveOrDrafts}
-              />
-            ))}
-          </ScrollView>
-        </View>
+    <View style={styles.container}>
+      <View style={styles.homeView}>
+        <Header />
+        <SearchBar />
+        <ScrollView style={styles.notesContainer}>
+          {items.map((item, index) => (
+            <NotesList
+              key={index}
+              personality={item.personality}
+              create_at={item.createAt}
+              notesStatus={item.saveOrDrafts}
+              description={item.description}
+            />
+          ))}
+        </ScrollView>
       </View>
-    </SafeAreaView>
-  );
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-  containerBox: {
-    flex: 1,
-    width: '100%',
-    paddingHorizontal: 25,
-  },
   container: {
     flex: 1,
     flexDirection: 'column',
     alignItems: 'flex-end',
     backgroundColor: '#050513',
+    width: '100%',
+    paddingHorizontal: 25,
+    paddingVertical: 45,
+  },
+  homeView: {
+    width: '100%',
   },
   notesContainer: {
     width: '100%',
     marginTop: 10,
   },
-});
+})
