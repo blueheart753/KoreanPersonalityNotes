ad9752594fd81297f57c3a32d45da03771fda670
kimminam <bluemind8520@gmail.com>
2024-07-02 23:42:37 +0900

    Feat : IOS 최적화 삭제
    SafeAreaView 삭제

--- koreanpersonalitynotes/components/navigation/RandomNote.tsx
+++ koreanpersonalitynotes/components/navigation/RandomNote.tsx
@@ -1,43 +1,40 @@
-import { StyleSheet, View, Text, SafeAreaView } from 'react-native';
-import { useEffect, useState } from 'react';
-import AllNotesButton from '../HomeButton';
-import WriteNote from '../WriteNote';
-import RandomKoreanPersonality from '@/api/KoreanPersonalityAPI';
+import { StyleSheet, View, Text, SafeAreaView } from 'react-native'
+import { useEffect, useState } from 'react'
+import AllNotesButton from '../HomeButton'
+import WriteNote from '../WriteNote'
+import RandomKoreanPersonality from '@/api/KoreanPersonalityAPI'
 
 const RandomNote = () => {
-  const [randomPersonality, setRandomPersonality] = useState<string>('');
+  const [randomPersonality, setRandomPersonality] = useState<string>('')
 
   useEffect(() => {
     const fatchData = async () => {
-      const data = await RandomKoreanPersonality();
+      const data = await RandomKoreanPersonality()
       if (data) {
-        setRandomPersonality(data);
+        setRandomPersonality(data)
       }
-    };
-    fatchData();
-  }, []);
+    }
+    fatchData()
+  }, [])
 
   return (
-    <SafeAreaView style={styles.RandomKeywordNoteWrap}>
-      <View style={styles.noteWrap}>
-        <AllNotesButton />
-        <View>
-          <WriteNote personality={randomPersonality} isRandom={true} />
-        </View>
+    <View style={styles.RandomKeywordNoteWrap}>
+      <AllNotesButton />
+      <View>
+        <WriteNote personality={randomPersonality} isRandom={true} />
       </View>
-    </SafeAreaView>
-  );
-};
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   RandomKeywordNoteWrap: {
     width: '100%',
     flex: 1,
+    paddingHorizontal: 25,
+    paddingVertical: 40,
     backgroundColor: '#050513',
   },
-  noteWrap: {
-    paddingHorizontal: 25,
-  },
-});
+})
 
-export default RandomNote;
+export default RandomNote
