ecc1bc6bbeae593952d03baed02143528e713bf4
kimminam <bluemind8520@gmail.com>
2024-07-02 09:30:23 +0900

    Feat. 아이폰 화면 최적화

--- koreanpersonalitynotes/components/navigation/RandomNote.tsx
+++ koreanpersonalitynotes/components/navigation/RandomNote.tsx
@@ -1,40 +1,43 @@
-import { StyleSheet, View, Text } from 'react-native'
-import { useEffect, useState } from 'react'
-import AllNotesButton from '../HomeButton'
-import WriteNote from '../WriteNote'
-import RandomKoreanPersonality from '@/api/KoreanPersonalityAPI'
+import { StyleSheet, View, Text, SafeAreaView } from 'react-native';
+import { useEffect, useState } from 'react';
+import AllNotesButton from '../HomeButton';
+import WriteNote from '../WriteNote';
+import RandomKoreanPersonality from '@/api/KoreanPersonalityAPI';
 
 const RandomNote = () => {
-  const [randomPersonality, setRandomPersonality] = useState<string>('')
+  const [randomPersonality, setRandomPersonality] = useState<string>('');
 
   useEffect(() => {
     const fatchData = async () => {
-      const data = await RandomKoreanPersonality()
+      const data = await RandomKoreanPersonality();
       if (data) {
-        setRandomPersonality(data)
+        setRandomPersonality(data);
       }
-    }
-    fatchData()
-  }, [])
+    };
+    fatchData();
+  }, []);
 
   return (
-    <View style={styles.RandomKeywordNoteWrap}>
-      <AllNotesButton />
-      <View>
-        <WriteNote personality={randomPersonality} isRandom={true} />
+    <SafeAreaView style={styles.RandomKeywordNoteWrap}>
+      <View style={styles.noteWrap}>
+        <AllNotesButton />
+        <View>
+          <WriteNote personality={randomPersonality} isRandom={true} />
+        </View>
       </View>
-    </View>
-  )
-}
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   RandomKeywordNoteWrap: {
     width: '100%',
     flex: 1,
     backgroundColor: '#050513',
-    paddingHorizontal: 25,
-    paddingVertical: 35,
   },
-})
+  noteWrap: {
+    paddingHorizontal: 25,
+  },
+});
 
-export default RandomNote
+export default RandomNote;
